a8b43170fa606431e336d3a36e703bf19f3e57cb
Marcos <araujo678@gmail.com>
2023-08-17 18:17:37 -0300

    Atividade 17

--- SECAO_4/main.py
+++ SECAO_4/main.py
@@ -1,18 +1,30 @@
-# Atividade 14
-x = str(3)
-y = int(4)
-z = float(5)
+# # Atividade 14
+# x = str(3)
+# y = int(4)
+# z = float(5)
 
-print(x + x)
-print(y + y)
-print(z + z)
+# print(x + x)
+# print(y + y)
+# print(z + z)
 
-# O marcos tem 26 anos de idade e mora em Pinhais
-idade  = input('Digite sua idade: ')
-idade = str(idade)
-nome = input("Digite seu nome: ")
-cidade = input("Digite sua cidade: ")
+# ####################################################################################################################################
 
-print("O " + nome + "tem " + idade + " anos de idade e mora em " + cidade)
+# # O marcos tem 26 anos de idade e mora em Pinhais
+# idade  = input('Digite sua idade: ')
+# idade = str(idade)
+# nome = input("Digite seu nome: ")
+# cidade = input("Digite sua cidade: ")
 
-#Atividade 16 
+# print("O " + nome + "tem " + idade + " anos de idade e mora em " + cidade)
+
+####################################################################################################################################
+
+#Atividade 17
+import datetime
+from datetime import datetime
+
+anoNascimento = input("Digite o seu ano de nascimento (XXXX): ")
+idade = str(int(datetime.now().year) - int(anoNascimento))
+
+print("A idade é: " + idade)
+
