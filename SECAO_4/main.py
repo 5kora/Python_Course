c3c7b7af215367c4cd97432bf57e467333412b86
Marcos <araujo678@gmail.com>
2023-08-17 18:07:23 -0300

    Aula 16

--- SECAO_4/main.py
+++ SECAO_4/main.py
@@ -1,3 +1,4 @@
+# Atividade 14
 x = str(3)
 y = int(4)
 z = float(5)
@@ -5,3 +6,13 @@ z = float(5)
 print(x + x)
 print(y + y)
 print(z + z)
+
+# O marcos tem 26 anos de idade e mora em Pinhais
+idade  = input('Digite sua idade: ')
+idade = str(idade)
+nome = input("Digite seu nome: ")
+cidade = input("Digite sua cidade: ")
+
+print("O " + nome + "tem " + idade + " anos de idade e mora em " + cidade)
+
+#Atividade 16 
