9e1bfb391b64a7acf1ae6d756fe08dee84f1ccfd
Marcos <araujo678@gmail.com>
2023-08-18 15:36:59 -0300

    Atividade 26

--- SECAO_4/main.py
+++ SECAO_4/main.py
@@ -19,12 +19,91 @@
 
 ####################################################################################################################################
 
-#Atividade 17
-import datetime
-from datetime import datetime
+# #Atividade 17
+# import datetime
+# from datetime import datetime
 
-anoNascimento = input("Digite o seu ano de nascimento (XXXX): ")
-idade = str(int(datetime.now().year) - int(anoNascimento))
+# anoNascimento = input("Digite o seu ano de nascimento (XXXX): ")
+# idade = str(int(datetime.now().year) - int(anoNascimento))
 
-print("A idade é: " + idade)
+# print("A idade é: " + idade + " anos.")
 
+####################################################################################################################################
+
+# # Atividade 18
+
+# fruta  = "Abacate"
+# numero = str(99.58)
+
+# # index
+# print(fruta[2:5])
+# print(fruta[2])
+# print(fruta[-1])
+
+# print(numero.split(".")[1])
+
+####################################################################################################################################
+
+# # Atividade 19
+
+# # Dolly guarana, saboroso e [delicioso]
+
+# refrigerante = "Dolly"
+# sabor = "guarana"
+# adjetivo = "delicioso"
+
+# texto = f'{refrigerante} {sabor}, saboroso e [{adjetivo}]'
+
+# print(texto)
+
+####################################################################################################################################
+
+# # Atividade 20
+
+# mensagem = "     Eu amo pizza de chocolate"
+
+# # Lowercase
+# print(mensagem.lower())
+# # Uppercase
+# print(mensagem.upper())
+# # Capitalize (ja esta assim a minha frase)
+# print(mensagem.capitalize())
+# # find letter (Se não encontrar retorna -1)
+# print(mensagem.find("a"))
+# #replace method
+# print(mensagem.replace("a","i"))
+# #strip method
+# print(mensagem.strip())
+
+
+####################################################################################################################################
+
+# # Atividade 24
+
+# velocidade = 35
+
+# if velocidade > 80:
+#     print("Acima da média patrao!")
+# elif velocidade < 80 and velocidade > 40 :
+#     print("Niceee")
+# else:
+#     print("Lerdinho hehehe")
+    
+# print("Fim!")
+
+####################################################################################################################################
+
+# # Atividade 25
+
+# rendaAcima5k = True
+
+# nomeLimpo = True
+
+# if rendaAcima5k and nomeLimpo:
+#     print("Financiamento aprovado!")
+# else:
+#     print("Financiamento negado!")
+
+####################################################################################################################################
+
+# Atividade 26
