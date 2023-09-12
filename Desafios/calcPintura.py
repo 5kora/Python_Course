6854c49462249c7db325b779c5fc6eb8eaf34c20
Marcos <araujo678@gmail.com>
2023-09-12 12:54:19 -0300

    Atividade 91

--- Desafios/calcPintura.py
+++ Desafios/calcPintura.py
@@ -2,7 +2,8 @@ rendimento = int(input('Qual é o rendimento da lata em m²? '))
 largura = int(input('Qual é a largura da parede m? '))
 altura = int(input('Qual é a altura da parede m? '))
 
-totalLatas = (largura * altura) / rendimento
+def calculoTinta():
+    totalLatas = (largura * altura) / rendimento
+    print(f'Voce precisa de {totalLatas} latas de tinta')
 
-print(f'Voce precisa de {totalLatas} latas de tinta')
-
+calculoTinta()
