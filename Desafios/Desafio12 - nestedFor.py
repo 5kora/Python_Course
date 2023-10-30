eaad45eb048630b42c70e9aafaa8e9c5d19514fc
Marcos <araujo678@gmail.com>
2023-10-30 16:05:56 -0300

    Desafio 13

---
+++ Desafios/Desafio12 - nestedFor.py
@@ -0,0 +1,6 @@
+frutas = ["maça", "banana", "manga", "uva", "abacaxi"]
+vegetais = ["alface", "repolho", "beterraba", "beringela", "cenoura"]
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(f'Fruta: {fruta} e Vegetal: {vegetal}')
