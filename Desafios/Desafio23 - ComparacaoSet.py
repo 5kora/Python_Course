075409c029a715cd6b5c45773548e264807577d1
Marcos <araujo678@gmail.com>
2023-11-10 14:02:42 -0300

    Desafio25

---
+++ Desafios/Desafio23 - ComparacaoSet.py
@@ -0,0 +1,6 @@
+amigos1 = {'Eduardo', 'Fabio', 'Leonardo', 'Marcello', 'Joao'}
+amigos2 = {'Eduardo', 'Fabio', 'Leonardo', 'Victor', 'Marcio'}
+
+intersectionNames = amigos1.intersection(amigos2)
+
+print(intersectionNames)
---
+++ Desafios/Desafio24 - FuncaoSimples.py
@@ -0,0 +1,6 @@
+def calcSquare(numero):
+    return numero ** 2
+
+num = int(input("Digite um numero: "))
+
+print(f'O quadrado de {num} é: {calcSquare(num)}')
